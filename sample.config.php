16c8b0f18af1415e0e7e390063ffb7e411a0fbbf
sheng <sheng@iastate.edu>
2017-09-05 11:08:35 -0500

    Added Sample config

---
+++ sample.config.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['osApiURL'] = "https://<domain>:8443/oapi/v1/";
+$config['kApiURL'] = "https://<domain>:8443/api/v1/";
+
+$config['osNamespace'] = "<Openshift Project Name>";
+$config['osAuthHeader'] = "<Auth header to send>";
